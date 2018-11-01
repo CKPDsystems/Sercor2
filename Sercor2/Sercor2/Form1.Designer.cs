4ce2574d5346818f0609eb214bca22198cf694dd
Ernesteins <eyaselga@hotmail.com>
2018-11-01 02:27:09 -0500

    gitignore

---
+++ Sercor2/Sercor2/Form1.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace Sercor2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.text_hola = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // text_hola
+            // 
+            this.text_hola.Location = new System.Drawing.Point(354, 25);
+            this.text_hola.Name = "text_hola";
+            this.text_hola.Size = new System.Drawing.Size(100, 20);
+            this.text_hola.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.text_hola);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox text_hola;
+    }
+}
+
